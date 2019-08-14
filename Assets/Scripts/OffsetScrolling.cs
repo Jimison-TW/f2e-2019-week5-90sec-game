b469e778976f3b871dbb7a670aee8b0018446130
Jimison-TW <ex3067878@gmail.com>
2019-08-15 01:00:52 +0800

    完成背景捲動

---
+++ Assets/Scripts/OffsetScrolling.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class OffsetScrolling : MonoBehaviour
+{
+  public float scrollSpeed;
+
+  private Renderer renderer;
+  private Vector2 savedOffset;
+
+  void Start()
+  {
+    renderer = GetComponent<Renderer>();
+    Debug.Log(renderer.name);
+  }
+
+  void Update()
+  {
+    float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+    Vector2 offset = new Vector2(x, 0);
+    renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+  }
+}
