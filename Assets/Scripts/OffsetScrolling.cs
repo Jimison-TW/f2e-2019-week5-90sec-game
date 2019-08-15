34f9e5c0803be159b93bfc267a582d4659f0a5fc
Jimison-TW <ex3067878@gmail.com>
2019-08-16 00:54:46 +0800

    player move complete

--- Assets/Scripts/OffsetScrolling.cs
+++ Assets/Scripts/OffsetScrolling.cs
@@ -2,21 +2,21 @@
 
 public class OffsetScrolling : MonoBehaviour
 {
-  public float scrollSpeed;
+    public float scrollSpeed;
 
-  private Renderer renderer;
-  private Vector2 savedOffset;
+    private Renderer renderer;
+    private Vector2 savedOffset;
 
-  void Start()
-  {
-    renderer = GetComponent<Renderer>();
-    Debug.Log(renderer.name);
-  }
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        Debug.Log(renderer.name);
+    }
 
-  void Update()
-  {
-    float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
-    Vector2 offset = new Vector2(x, 0);
-    renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
-  }
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, 0);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.position.y < 1 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            player.Translate(new Vector2(0, 1));
+        }
+        else if (player.position.y > -2 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            player.Translate(new Vector2(0, -1));
+        }
+
+        if (player.position.x < 8 && Input.GetKey(KeyCode.RightArrow))
+        {
+            player.Translate(Vector3.right * Time.deltaTime * 5);
+        }
+        else if (player.position.x > -8 && Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.Translate(Vector3.left * Time.deltaTime * 5);
+        }
     }
 }
